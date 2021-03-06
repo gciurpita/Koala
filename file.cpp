21cee64a0efb26216ef1c3dfd8c0600a50819cde
greg ciurpita <greg.ciurpita@gmail.com>
2021-03-06 15:29:26 -0500

    added fileDelete ()

--- file.cpp
+++ file.cpp
@@ -2,7 +2,9 @@
 #include "FS.h"
 
 #include "file.h"
+#include "koala.h"
 #include "vars.h"
+#include "utils.h"
 
 const char *filename = "/spiffs/Koala/koala.dat";
 File  file;
@@ -20,6 +22,38 @@ fileDir (void)
     while ((f = root.openNextFile ()))  {
         Serial.println (f.name ());
     }
+}
+
+// ---------------------------------------------------------
+// delete selected file
+void
+fileDelete (void)
+{
+    Serial.println (__func__);
+
+    File root = SPIFFS.open ("/");
+    File f;
+
+    while ((f = root.openNextFile ()))  {
+        // display filename
+        printf ("  %s:\n", f.name ());
+
+        // wait for keypress
+        readLine (s, S_SIZE);
+        if ('\n' == s [0])  {
+            continue;
+        }
+
+        // wait for confirmation
+        printf ("   confirm deletion of %s: (D)\n", f.name ());
+        readLine (s, S_SIZE);
+        if ('D' == s [0])  {
+            printf ("    deleting %s\n", f.name());
+            SPIFFS.remove (f.name ());
+        }
+    }
+
+    printf (" %s: done\n", __func__);
 }
 
 // ---------------------------------------------------------
