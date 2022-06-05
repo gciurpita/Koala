ed0538b138ce77a23cb3f9b22895857fbc452da3
greg ciurpita <greg.ciurpita@gmail.com>
2022-06-05 18:51:41 -0400

    check for change to allow setting thru cmds

--- pots.cpp
+++ pots.cpp
@@ -14,6 +14,14 @@ potsRead (void)
     slope    = 100 * analogRead (ADC_SLOPE)   / ADC_MAX;
     whistle  = 100 * analogRead (ADC_WHISTLE) / ADC_MAX;
     reverser = 100 * analogRead (ADC_REV)     / ADC_MAX;
-    throttle = 100 * analogRead (ADC_THR)     / ADC_MAX;
+
+    // capture analog changes allowing cmds to overwrite
+    static int thrLst;
+           int thr = 100 * analogRead (ADC_THR)     / ADC_MAX;
+
+    if (thrLst != thr)  {
+        thrLst   = thr;
+        throttle = 100 * thr / ADC_MAX;
+    }
 }
 
