7a476469f168bc9064b42d3ca58d885076597cf6
greg ciurpita <greg.ciurpita@gmail.com>
2022-06-12 19:09:28 -0400

    added potChange() to only update changes allowing sim cmds

--- pots.cpp
+++ pots.cpp
@@ -7,19 +7,32 @@
 
 #define ADC_MAX   4095
 
+static int _sloLst;
+static int _whiLst;
+static int _revLst;
+static int _thrLst;
+
+// -----------------------------------------------------------------------------
+// only update changes allowing cmds to overwrite
+void
+potChange (
+    int  & var,
+    int  & valLst,
+    byte  pin )
+{
+    int val = 100 * analogRead (pin) / ADC_MAX;
+
+    if (valLst != val)
+        var = valLst = val;
+}
+
 // -----------------------------------------------------------------------------
 void
 potsRead (void)
 {
-    slope    = 100 * analogRead (ADC_SLOPE)   / ADC_MAX;
-    whistle  = 100 * analogRead (ADC_WHISTLE) / ADC_MAX;
-    reverser = 100 * analogRead (ADC_REV)     / ADC_MAX;
-
-    // capture analog changes allowing cmds to overwrite
-    static int thrLst;
-           int thr = 100 * analogRead (ADC_THR)     / ADC_MAX;
-
-    if (thrLst != thr)
-        throttle = thrLst   = thr;
+    potChange (slope,    _sloLst, ADC_SLOPE);
+    potChange (whistle,  _whiLst, ADC_WHISTLE);
+    potChange (reverser, _revLst, ADC_REV);
+    potChange (throttle, _thrLst, ADC_THR);
 }
 
